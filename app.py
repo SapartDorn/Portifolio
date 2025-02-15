e2e119813787df80861a337a425c2f7a820ec172
SapartDorn <leonardohlss14@gmail.com>
2025-02-15 14:38:16 -0300

    ajustes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, make_response, redirect, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='.')
 
 @app.route('/')
 def index():
     theme = request.cookies.get('theme', 'dark')  # tema padrão é dark
-    return render_template('resume.html', theme=theme)
+    return render_template('index.html', theme=theme)
 
 @app.route('/toggle-theme')
 def toggle_theme():
