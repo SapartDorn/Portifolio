bc082a73e8bceae83463f74fa5ee5189c9928f6e
SapartDorn <leonardohlss14@gmail.com>
2025-02-15 13:55:18 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, make_response, redirect, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    theme = request.cookies.get('theme', 'dark')  # tema padrão é dark
+    return render_template('resume.html', theme=theme)
+
+@app.route('/toggle-theme')
+def toggle_theme():
+    response = make_response(redirect('/'))
+    current_theme = request.cookies.get('theme', 'dark')
+    new_theme = 'light' if current_theme == 'dark' else 'dark'
+    response.set_cookie('theme', new_theme)
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
